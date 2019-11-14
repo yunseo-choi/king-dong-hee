e97b1294dd21ec0d93ed4ab8c70e6564ac9bd9bc
Yunseo Choi <54052683+yunseo-choi@users.noreply.github.com>
2019-11-14 14:02:29 +0900

    Update 소스.cpp

---
+++ king dong hee/소스.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int i;
+	int j;
+	for (i = 1; i <= 5; i++) {
+		for (j = 1; i <= j; j++){
+			printf("%d", j);
+	}
+	}printf("\n");
+	
+	}
